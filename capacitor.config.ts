a6a9f1279fec435a36aec57f092d8e6f896358a5
Elle'Vera <lyod.knyazh@gmail.com>
2025-11-29 00:31:21 +0530

    2nd

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.AnimeApp',
+  appName: 'AnimeApp',
+  webDir: 'anime-app'
+ 
+};
+
+export default config;
